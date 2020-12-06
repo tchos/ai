63cb48e28e69b685cb43545b011f94051a5de028
tchos <kwenol@yahoo.fr>
2020-12-06 13:15:14 +0100

    BD prête pour le contentieux

---
+++ update.sql
@@ -0,0 +1,8 @@
+UPDATE regul_rev_susp SET `qualite_ayant_droit` = '', `date_nais_der_orph` = NULL, `matricule_auteur` = '', `date_deces` = NULL, `noms_ad_acte` = '', `num_acte_revers` = '', `type_acte` = '', `date_signature_rev` = NULL, `date_regul` = NULL, `regulariser_y_n` = 0 WHERE id = 4426 ;
+
+UPDATE regul_rev_clo SET `qualite_ayant_droit` = '', `date_nais_der_orph` = NULL, `matricule_auteur` = '', `date_deces` = NULL, `noms_ad_acte` = '', `num_acte_revers` = '', `type_acte` = '', `date_signature_rev` = NULL, `date_regul` = NULL, `regulariser_y_n` = 0 WHERE id = 11129 ;
+
+UPDATE regul_inv_susp SET `type_acte` = '', `date_signature` = NULL, `nom_inv_acte` = '', `date_invalidite` = NULL, `date_regul` = NULL, `regulariser_y_n` = 0, `num_acte_inval` = '', `date_nais_der_orph` = NULL WHERE id = 182 ;
+
+UPDATE regul_inv_clo SET `type_acte` = '', `date_signature` = NULL, `nom_inv_acte` = '', `date_invalidite` = NULL, `date_regul` = NULL, `regulariser_y_n` = 0, `num_acte_inval` = '', `date_nais_der_orph` = NULL WHERE id = 98 ;
+
